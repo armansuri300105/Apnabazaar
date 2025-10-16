3d5a7d41d3aa64cd820d96cfbc238d0c290710ef
armansuri300105 <arshadmansuri572@gmail.com>
2025-10-16 22:22:16 +0530

    Fixing error of update order status

--- admin/API/product.jsx
+++ admin/API/product.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    // baseURL: "https://apnabazaar-backend-3iwt.onrender.com",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://apnabazaar-backend-3iwt.onrender.com",
     withCredentials: true
 })
 
